828e88fe0aaef1c7edcaf2676d080ec0323d6b4c
Saviour <133455623+Svrjn@users.noreply.github.com>
2023-05-18 16:28:23 +0100

    Update Python_basic_1.py

--- Python_basic_1.py
+++ Python_basic_1.py
@@ -3,4 +3,4 @@ name=input("what is your name\t")
 age=int(input("how old are you?\t"))
 #your age should be only in numeric form,e.g 10
 phone=int(input("Enter your phone number\t"))
-print("Hello {}, you are {} years old with the phone number {}".format(name,age,phone))
+print("Hello {}, you are {} years old with the phone number ({})".format(name,age,phone))
