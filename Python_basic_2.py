665a5699ae5905c7def9962f3ab101025d0a6247
Saviour <133455623+Svrjn@users.noreply.github.com>
2023-05-18 07:29:22 +0100

    Create Python_basic_2.py
    Assignment number 2

---
+++ Python_basic_2.py
@@ -0,0 +1,12 @@
+#program to swap numbers
+num1=int(input("enter your first number:\t"))
+
+num2=int(input("enter the second number:\t"))
+
+#swapping the numbers
+
+num1,num2=num2,num1
+
+print("value of first number after swapping is: {}".format(num1))
+
+print("value of second number after swapping is:{}".format(num2))
