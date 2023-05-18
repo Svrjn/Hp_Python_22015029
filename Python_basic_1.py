c6a60a4baccfca7d387d3884ba273eed8def5ab2
Saviour <133455623+Svrjn@users.noreply.github.com>
2023-05-18 07:16:54 +0100

    Create Python_basic_1.py
    Assignment number 1

---
+++ Python_basic_1.py
@@ -0,0 +1,6 @@
+#program to display name,age and phone number
+name=input("what is your name\t")
+age=int(input("how old are you?\t"))
+#your age should be only in numeric form,e.g 10
+phone=int(input("Enter your phone number\t"))
+print("Hello {}, you are {} years old with the phone number {}".format(name,age,phone))
