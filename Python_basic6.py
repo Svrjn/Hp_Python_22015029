56cd6276013a5beee908adb5e0649fb65bd35cc8
Saviour <133455623+Svrjn@users.noreply.github.com>
2023-05-22 06:43:41 +0100

    Python_basic6.py

---
+++ Python_basic6.py
@@ -0,0 +1,6 @@
+#program to test even and odd numbers.
+numb=int(input("Enter a number\t"))#only integers
+if numb%2==0:
+    print("{} is an even number.".format(numb))
+else:
+    print("{} is an odd number.".format(numb))
