336227c6720fe7df3a5828aa4e973da34f0fde54
Saviour <133455623+Svrjn@users.noreply.github.com>
2023-05-22 11:48:50 +0100

    Python _basic4.py

--- Python _basic4.py
+++ Python _basic4.py
@@ -1,11 +1,13 @@
 #program to determine leap year
-
-year=int(input("Enter year\t"))
-
-if year%4==0:
-
+year=int(input("Enter year to be checked:\t"))
+if(year%4==0 and year%100!=0 or year%400==0):
     print("{} is a leap year.".format(year))
-
 else:
-
     print("{} is a lunar year.".format(year))
+
+
+
+
+
+
+
